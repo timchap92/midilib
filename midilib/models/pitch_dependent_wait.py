f0575e364acc25d8611a178f70c94aa4c5dc568d
tim <tim@aardvark.com.au>
2019-06-30 23:26:05 +0200

    Finish next note prediction

--- midilib/models/pitch_dependent_wait.py
+++ midilib/models/pitch_dependent_wait.py
@@ -7,18 +7,18 @@ from midilib.featuring.util import normalize_song
 
 class PitchDependentWaitModel:
 
-    def __init__(self, notebased_featurer, pitch_model, pitch_dependent_wait_model):
+    def __init__(self, notebased_featurer, pitch_model, wait_model):
         """
 
         Parameters
         ----------
         notebased_featurer : NoteBasedFeaturer
         pitch_model : keras.models.Model
-        pitch_dependent_wait_model : keras.models.Model
+        wait_model : keras.models.Model
         """
         self.notebased_featurer = notebased_featurer
         self.pitch_model = pitch_model
-        self.pitch_dependent_wait_model = pitch_dependent_wait_model
+        self.wait_model = wait_model
 
     def dump(self, path):
         os.mkdir(path)
@@ -26,7 +26,7 @@ class PitchDependentWaitModel:
             os.path.join(path, 'pitch_dependent_wait_featurer'))
         self.pitch_model.save(
             os.path.join(path, 'pitch_model'))
-        self.pitch_dependent_wait_model.save(
+        self.wait_model.save(
             os.path.join(path, 'pitch_dependent_wait_model'))
 
     @classmethod
@@ -43,4 +43,8 @@ class PitchDependentWaitModel:
     def next_note(self, notes):
         notes = normalize_song(notes)
         fnotes = self.notebased_featurer.feature_notes(notes)
+        sequence = self.notebased_featurer.extract_sequence_for_prediction(fnotes)
+        pitch = self.pitch_model.predict(sequence)
+        wait = self.wait_model.predict([sequence, pitch])
+        return pitch[0], wait[0]
 
